d5622c92c45db6e51db311e0334d8b2a82561715
rng <mancng@gmail.com>
2018-02-14 17:23:52 -0800

    complete the setup of service and observables for angular

--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpService } from './http.service';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [HttpService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
